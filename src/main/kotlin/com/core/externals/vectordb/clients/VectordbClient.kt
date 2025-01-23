c08668db4c5046aedca49a8c1e95e7392c8bd8d2
Oscar Arellano <arellanooscar.ing@gmail.com>
2025-01-22 21:07:44 -0500

    change prefix vector-db

--- src/main/kotlin/com/core/externals/vectordb/clients/VectordbClient.kt
+++ src/main/kotlin/com/core/externals/vectordb/clients/VectordbClient.kt
@@ -10,14 +10,14 @@ import reactor.core.publisher.Mono
 
 @Client("vectordb")
 interface VectordbClient {
-    @Post("/vector-db/search/{providerDB}/{indexName}")
+    @Post("/api/ms/vector-db/search/{providerDB}/{indexName}")
     fun search(
         providerDB: String,
         indexName: String,
         @Body queryRequest: QueryRequest
     ): Mono<List<SearchResponse>>
 
-    @Post("/vector-db/upsert_data/{providerDB}/{indexName}")
+    @Post("/api/ms//vector-db/upsert_data/{providerDB}/{indexName}")
     fun upsertData(
         providerDB: String,
         indexName: String,
